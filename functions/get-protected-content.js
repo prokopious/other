25192a9117b503fa403a34fa56a5d6216c3892bf
tmhuyett <toddhuyett@gmail.com>
2020-11-27 22:20:25 -0500

    'dddd

--- functions/get-protected-content.js
+++ functions/get-protected-content.js
@@ -10,22 +10,7 @@ const content = {
     allowedRoles: ['free', 'pro', 'premium'],
   },
 
-  free:  [
-    { "name": "Apple", "src": "/images/apple.jpg" },
-    { "name": "Apple2", "src": "/images/apple2.jpg" },
-    { "name": "Apple3", "src": "/images/apple3.jpg" },
-    { "name": "Apple4", "src": "/images/apple4.jpg" },
-    { "name": "Apple5", "src": "/images/apple5.jpg" },
-    { "name": "Apple6", "src": "/images/apple6.jpg" },
-    { "name": "Bagels", "src": "/images/bagels.jpg" },
-    { "name": "Single bagel", "src": "/images/single-bagel.jpg" },
-    { "name": "Carrots", "src": "/images/carrots.jpg" },
-    { "name": "Latte art", "src": "/images/latte-art.jpg" },
-    { "name": "Noodles", "src": "/images/noodles.jpg" },
-    { "name": "Branches", "src": "/images/branches.png" }
-],
-
-  pro: {
+  free: {
     src:
       'https://images.unsplash.com/photo-1519098901909-b1553a1190af?auto=format&fit=crop&w=600&h=600&q=80',
     alt: 'close-up of a corgi with its tongue hanging out',
@@ -35,7 +20,18 @@ const content = {
       'This is protected content! It’s only available if you have a pro plan or higher.',
     allowedRoles: ['free','pro', 'premium'],
   },
-  premium: {
+
+  free: {
+    src:
+      'https://images.unsplash.com/photo-1519098901909-b1553a1190af?auto=format&fit=crop&w=600&h=600&q=80',
+    alt: 'close-up of a corgi with its tongue hanging out',
+    credit: 'Florencia Potter',
+    creditLink: 'https://unsplash.com/photos/yxmNWxi3wCo',
+    message:
+      'This is protected content! It’s only available if you have a pro plan or higher.',
+    allowedRoles: ['free','pro', 'premium'],
+  },
+   four: {
     src:
       'https://images.unsplash.com/photo-1546975490-e8b92a360b24?auto=format&fit=crop&w=600&h=600&q=80',
     alt: 'corgi in a tent with string lights in the foreground',
@@ -43,9 +39,9 @@ const content = {
     creditLink: 'https://unsplash.com/photos/cX-KEISwDIw',
     message:
       'This is protected content! It’s only available if you have the premium plan.',
-    allowedRoles: ['premium'],
-  },
-};
+    allowedRoles: ['free'],
+  },}
+
 
 exports.handler = async (event, context) => {
   const { type } = JSON.parse(event.body);
